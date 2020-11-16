ca7de4a1f84ea9563dd77ed31584ac9108788b6e
Bruce Jernell <59374529+opconWizard@users.noreply.github.com>
2020-11-16 15:47:51 -0500

    Added documentation

--- 11-16-2020_MAS-And-Docusign/OpConKeywordSearch.sql
+++ 11-16-2020_MAS-And-Docusign/OpConKeywordSearch.sql
@@ -1,4 +1,19 @@
-USE OPCON
+-- This script searches OpCon jobs for the entered "keyword"
+-- and displays the results with the schedule/job/job value
+-- where it was found.
+
+-- A good use case would be a Self Service button with an input
+-- text search, that emails or creates an output file of the 
+-- results.
+
+-- To use the script, replace <DB> with the name of your OpCon
+-- database.
+
+-- Also replace <keyword> with a keyword that you want to find in
+-- an OpCon job. 
+------------------------------------------------------------------
+
+USE <DB>
 
 SELECT 
 SNAME.SKDNAME AS [SCHEDULE]
